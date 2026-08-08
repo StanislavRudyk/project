a0bb578418fb673ce45b34b7dc8879437ad4b8f3
WizzyWodich <wizzywodich@gmail.com>
2026-08-08 22:04:11 +0300

    Add: LogoutAll system

---
+++ backend/BookShare.Core/Endpoints/Auth/Logout/LogoutHandler.cs
@@ -0,0 +1,6 @@
+namespace BookShare.Core.Endpoints.Auth.Logout;
+
+public class LogoutHandler
+{
+    
+}
---
+++ backend/BookShare.Core/Endpoints/Auth/LogoutAll/LogoutAllEndpoint.cs
@@ -0,0 +1,6 @@
+namespace BookShare.Core.Endpoints.Auth.LogoutAll;
+
+public class LogoutAllEndpoint
+{
+    
+}
